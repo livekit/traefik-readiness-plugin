167cd49b87d7e7b632965c7b1e0544d9fc899a00
Mathew Kamkar <578302+matkam@users.noreply.github.com>
2024-04-18 14:08:26 -0700

    fix package name

--- readiness.go
+++ readiness.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package traefikreadinessplugin
+package traefik_readiness_plugin
 
 import (
 	"context"
